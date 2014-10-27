4b857fc181e3a534a2d8e09912e75b0c9cf951ab
Tomo Masakura <tomo.masakura@gmail.com>
2014-10-27 19:48:23 +0900

    exec experimental.

--- spec/test_spec.js
+++ spec/test_spec.js
@@ -1,9 +1,15 @@
 (function () {
   'use strict';
 
+  var exec = require('child_process').exec;
+
   describe('Test demo', function () {
     it('test1', function () {
       expect(3).toEqual(3);
+    });
+
+    it('test2', function (done) {
+      exec('echo $PATH', done);
     });
   });
 })();
